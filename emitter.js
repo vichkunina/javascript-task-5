17954b029a3ee5e546821010bdc97a737a52f311
Daria <vichkunina@bk.ru>
2017-11-02 14:18:46 +0500

    Первый коммит

--- emitter.js
+++ emitter.js
@@ -1,68 +1,44 @@
 'use strict';
 
-/**
- * Сделано задание на звездочку
- * Реализованы методы several и through
- */
-getEmitter.isStar = true;
+getEmitter.isStar = false;
 module.exports = getEmitter;
 
-/**
- * Возвращает новый emitter
- * @returns {Object}
- */
 function getEmitter() {
+    var sub = {};
+
     return {
-
-        /**
-         * Подписаться на событие
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (!sub.hasOwnProperty(event)) {
+                sub[event] = [];
+            }
+            sub[event].push({
+                context, handler
+            });
+
+            return this;
         },
 
-        /**
-         * Отписаться от события
-         * @param {String} event
-         * @param {Object} context
-         */
         off: function (event, context) {
-            console.info(event, context);
+            for (let element in sub) {
+                if (element.includes(event + '.') || element === event) {
+                    sub[element] = sub[element].filter(current => current.context !== context);
+                }
+            }
+
+            return this;
         },
 
-        /**
-         * Уведомить о событии
-         * @param {String} event
-         */
         emit: function (event) {
-            console.info(event);
-        },
+            var eventList = event.split('.');
+            while (eventList.length > 0) {
+                event = eventList.join('.');
+                if (sub[event]) {
+                    sub[event].forEach(element => element.handler.call(element.context));
+                }
+                eventList.pop();
+            }
 
-        /**
-         * Подписаться на событие с ограничением по количеству полученных уведомлений
-         * @star
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         * @param {Number} times – сколько раз получить уведомление
-         */
-        several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
-        },
-
-        /**
-         * Подписаться на событие с ограничением по частоте получения уведомлений
-         * @star
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         * @param {Number} frequency – как часто уведомлять
-         */
-        through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            return this;
         }
     };
 }
